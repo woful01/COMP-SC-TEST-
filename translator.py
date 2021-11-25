0703246f12818a8b041ca695e9724f26ac91dd38
Vedat-Esendag <vedatesendag2005@gmail.com>
2021-11-25 22:26:05 +0200

    Create translator.py
    ustupid

---
+++ translator.py
@@ -0,0 +1,17 @@
+def reverseLookup(data,value):
+    keys[]
+    for key in data:
+        if data[key]==value:
+            keys.append(key)
+    return
+def main():
+    frEn={"le":"the","la":"the","livre":"book","pomme":"apple"}
+    print("the french word for 'the' are:",reverseLoop(frEn,"the"))
+    print()
+
+
+
+
+
+
+
